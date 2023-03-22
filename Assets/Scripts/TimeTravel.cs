3c80da437cafba9d338dbec78517d6814123b276
mikechi702 <mikechicarroll702@gmail.com>
2023-03-22 14:37:25 -0500

    finished time travel controls. implementation in levels are now needed

--- Assets/Scripts/TimeTravel.cs
+++ Assets/Scripts/TimeTravel.cs
@@ -8,6 +8,8 @@ public class TimeTravel : MonoBehaviour
     private GameObject userObject;
 
     private int timeSlots;
+    private float startTime;
+    private bool isTravelling = false;
     private InputAction timeLeap;
 
     private TimeTravelling timeTravel;
@@ -32,31 +34,38 @@ public class TimeTravel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //PSUEDOCODE FOR TIME TRAVEL SCRIPT
+        if(isTravelling)
+        {
+            if(Input.GetKey(KeyCode.Alpha1)) //turn this into a switch statement at some point for optimization
+            {
+                Debug.Log("Travelling to the past");
+                isTravelling = false;
+            }
+            else if(Input.GetKey(KeyCode.Alpha2))
+            {
+                Debug.Log("Travelling to the present");
+                isTravelling = false;
+            }
+            else if(Input.GetKey(KeyCode.Alpha3))
+            {
+                Debug.Log("Travelling to the future");
+                isTravelling = false;
+            }
 
-        // if(Input.GetKeyDown(KeyCode.LeftShift))
-        // {
-        //     Debug.Log("Time travel started");
-        //     if(Input.GetKeyUp(KeyCode.Alpha1))
-        //     {
-        //         Debug.Log("Travelling to past");
-        //     }
-        //     else if(Input.GetKey(KeyCode.Alpha2))
-        //     {
-        //         Debug.Log("Travelling to Present");
-        //     }
-        //     else if(Input.GetKey(KeyCode.Alpha3))
-        //     {
-        //         Debug.Log("Travelling to Future");
-        //     }
-        // }
-
-        
+            if(Time.time - startTime > 3.0) //aborts time travel if 3 seconds have passed
+            {
+                isTravelling = false;
+                Debug.Log("Time travel function stopped");
+            }
+        }
     }
 
     private void TimeSlotLeap(InputAction.CallbackContext timeContext)
     {
+        startTime = Time.time; //logs the time
         Debug.Log("Time travel initiated");
+        isTravelling = true;
+
     }
 
 }
