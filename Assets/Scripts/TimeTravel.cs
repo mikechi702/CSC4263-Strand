676777b53a1a06ae6135ccffe4034f210fa82eac
mikechi702 <mikechicarroll702@gmail.com>
2023-03-23 16:13:46 -0500

    --WIP-- Working on implementing time travel script in a sample skeleton level

--- Assets/Scripts/TimeTravel.cs
+++ Assets/Scripts/TimeTravel.cs
@@ -5,21 +5,23 @@ using UnityEngine.InputSystem;
 public class TimeTravel : MonoBehaviour
 {
     [SerializeField]
-    private GameObject userObject;
+    private GameObject userObject; //The player object being time travelled
+    private string currentTime; //the timeslot the player is currently in
+    private float startTime; //The time recorded when the time travel controls (Shift) starts.
+    private Vector2 currentPos; //Position of the player in current timeslot when time travel is executed
+    private Vector2[] playerPos = new Vector2[3]; //array containing all the player positions during time travel.
+    private bool isTravelling = false; //boolean used to determine if the player is within the timeLeap state
+    private InputAction timeLeap; //inputactions that enables the timeLeap state
 
-    private int timeSlots;
-    private float startTime;
-    private bool isTravelling = false;
-    private InputAction timeLeap;
+    private TimeTravelling timeTravel; //awakens the time travel object.
 
-    private TimeTravelling timeTravel;
-
-    private void Awake() 
+    private void Awake() //called at the beginning of the scene i think
     {
         timeTravel = new TimeTravelling();
+        currentTime = "Present";
     }
 
-    private void OnEnable() 
+    private void OnEnable() //execution of the time travel mechanic.
     {
         timeLeap = timeTravel.Player.TimeSlotLeap;
         timeLeap.Enable();
@@ -36,19 +38,22 @@ public class TimeTravel : MonoBehaviour
     {
         if(isTravelling)
         {
-            if(Input.GetKey(KeyCode.Alpha1)) //turn this into a switch statement at some point for optimization
+            if(Input.GetKey(KeyCode.Alpha1) && !currentTime.Equals("Past")) //turn this into a switch statement at some point for optimization
             {
                 Debug.Log("Travelling to the past");
+                currentTime = "Past";
                 isTravelling = false;
             }
-            else if(Input.GetKey(KeyCode.Alpha2))
+            else if(Input.GetKey(KeyCode.Alpha2) && !currentTime.Equals("Present"))
             {
                 Debug.Log("Travelling to the present");
+                currentTime = "Present";
                 isTravelling = false;
             }
-            else if(Input.GetKey(KeyCode.Alpha3))
+            else if(Input.GetKey(KeyCode.Alpha3) && !currentTime.Equals("Future"))
             {
                 Debug.Log("Travelling to the future");
+                currentTime = "Future";
                 isTravelling = false;
             }
 
@@ -62,10 +67,9 @@ public class TimeTravel : MonoBehaviour
 
     private void TimeSlotLeap(InputAction.CallbackContext timeContext)
     {
-        startTime = Time.time; //logs the time
+        startTime = Time.time; //logs the time to measure
         Debug.Log("Time travel initiated");
         isTravelling = true;
-
     }
 
 }
