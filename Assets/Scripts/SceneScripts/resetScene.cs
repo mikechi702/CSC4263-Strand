da96dc86bffadbeb0ae29334593cb76b0e6a9f58
smirkyface <72003137+smirkyface@users.noreply.github.com>
2023-03-27 22:52:17 -0500

    Update resetScene.cs

--- Assets/Scripts/SceneScripts/resetScene.cs
+++ Assets/Scripts/SceneScripts/resetScene.cs
@@ -7,38 +7,55 @@ public class resetScene : MonoBehaviour
 {
     private Scene currentScene;
     private bool resetting = false;
-    private float startTime = 0.0f;
+    private float startTime;
 
-    private void Awake() {
+    private void Awake()
+    {
         currentScene = SceneManager.GetActiveScene();
         resetting = false;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             Debug.Log("Reset scene state initialized");
             resetting = true;
             startTime = Time.time;
         }
-        if(Input.GetKeyDown(KeyCode.L) && resetting == true)
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-            SceneManager.LoadScene(currentScene.name); //resets the level just in case...
+            Debug.Log("Reset scene state stopped");
+            resetting = false;
+        }
+        if (Input.GetKeyDown(KeyCode.L) && resetting == true)
+        {
+            StartCoroutine(LoadLevelAsync(currentScene.name)); //resets the level just in case...
             Debug.Log("Scene reset");
         }
 
-        if(Time.time - startTime > 3.0 && startTime != 0.0f) //aborts time travel if 3 seconds have passed
+        if (Time.time - startTime > 3.0) //aborts time travel if 3 seconds have passed
         {
             resetting = false;
             Debug.Log("Scene reset aborted");
-            startTime = 0.0f;
+        }
+    }
+
+    IEnumerator LoadLevelAsync(string scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log("Loading progress: " + (asyncLoad.progress * 100) + "%");
+
+            yield return null;
         }
     }
 }
