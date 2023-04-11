0a24f181a801442b8489f14145559df46ac99e83
mikechi702 <mikechicarroll702@gmail.com>
2023-04-11 15:07:54 -0500

    Revert "Update portalScript.cs"
    This reverts commit 2cc3a97a2f14b3faa6d21bf42028e967445a3c2e.

--- Assets/Scripts/SceneScripts/portalScript.cs
+++ Assets/Scripts/SceneScripts/portalScript.cs
@@ -5,33 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class portalScript : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) 
     {
         Scene currentScene = SceneManager.GetActiveScene();
 
-        if (other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))
         {
-            switch (currentScene.name) //Whenever new level is added, ADD IT TO THHE BUILD SETTINGS !!!
+            switch(currentScene.name) //Whenever new level is added, ADD IT TO THHE BUILD SETTINGS !!!
             {
-                case ("SampleScene"):
-                    StartCoroutine(LoadLevelAsync("EndScreen"));
+                case("SampleScene"):
+                    SceneManager.LoadScene("EndScreen");
                     break;
-                case ("SampleScene2"):
-                    StartCoroutine(LoadLevelAsync("TitleScreen"));
+                case("SampleScene2"):
+                    SceneManager.LoadScene("TitleScreen");
                     break;
             }
-        }
-    }
-
-    IEnumerator LoadLevelAsync(string scene)
-    {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
-
-        while (!asyncLoad.isDone)
-        {
-            Debug.Log("Loading progress: " + (asyncLoad.progress * 100) + "%");
-
-            yield return null;
         }
     }
 }
