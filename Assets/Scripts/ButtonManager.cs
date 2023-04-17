871baa73f9d368fe49639088ed3bc618098c1b9f
smirkyface <72003137+smirkyface@users.noreply.github.com>
2023-04-17 18:11:37 -0500

    Update ButtonManager.cs

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,17 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public GameObject pause;
+    private Scene currentScene;
+
     public void StartGame() {
-        StartCoroutine(LoadSampleSceneAsync());
+        StartCoroutine(LoadSceneAsync("SampleScene"));
     }
 
     public void QuitGame() {
-        Debug.Log("Game successfully exited");
         Application.Quit();
     }
 
-    IEnumerator LoadSampleSceneAsync() {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
+    public void PauseGame() {
+        Time.timeScale = 0;
+        gameObject.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1;
+        transform.parent.gameObject.SetActive(false);
+        pause.SetActive(true);
+    }
+
+    public void RestartLevel() {
+        currentScene = SceneManager.GetActiveScene();
+        StartCoroutine(LoadSceneAsync(currentScene.name));
+    }
+
+    public void QuitLevel() {
+        StartCoroutine(LoadSceneAsync("TitleScreen"));
+    }
+
+    IEnumerator LoadSceneAsync(string scene) {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        Time.timeScale = 1;
 
         while (!asyncLoad.isDone) {
             Debug.Log("Loading progress: " + (asyncLoad.progress * 100) + "%");
