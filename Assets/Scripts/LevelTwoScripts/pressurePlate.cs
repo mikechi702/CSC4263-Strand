1c0354ab071f7e46424ea3acf693054666f8deb6
mikechi702 <mikechicarroll702@gmail.com>
2023-04-16 21:41:48 -0500

    70% of level 2 is done

---
+++ Assets/Scripts/LevelTwoScripts/pressurePlate.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlate : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject plate; //pressure plate gameobject
+    [SerializeField]
+    private float pressWeight = -0.1f; //speed which the pressure plate presses
+    private Vector3 initialPos; //starting point of the pressure plate
+    private bool moveUp = false; //determines if pressure plate needs to move back up
+    private float startTime, gravTime; //time buffer if pressure plate moves up
+    [SerializeField]
+    private GameObject pastCube, presentCube, futureCube; //the cubes that spawn when pressure plate is down
+    private Rigidbody2D presCubeRb;
+    private bool wasReversed = true;
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(plate.name == "PastPlate")
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                moveUp = false;
+                other.transform.parent = transform;
+                //plate.GetComponent<SpriteRenderer>().color = Color.red;
+                Debug.Log("Standing on plate");
+
+                pastCube.SetActive(true);
+                presentCube.SetActive(true);
+                futureCube.SetActive(true);
+            }
+        }
+        else if(plate.name == "PresentPlateGrav")
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+                other.transform.parent = transform;
+                //plate.GetComponent<SpriteRenderer>().color = Color.red;
+                Debug.Log("Standing on plate");
+                if(presentCube.activeSelf)
+                {
+                    presCubeRb.gravityScale *= -2; //reverses gravity when player steps on this plate.
+                }
+                else
+                    Debug.Log("Present cube not spawned!");
+                moveUp = false;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+            //plate.GetComponent<SpriteRenderer>().color = Color.green;
+            Debug.Log("Stepping off plate");
+
+            if(plate.name == "PresentPlateGrav")
+            {
+                presCubeRb.gravityScale *= -0.5f;
+                wasReversed = true;
+            }
+
+            startTime = Time.time;
+            moveUp = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            plate.transform.Translate(0,pressWeight,0);
+            Debug.Log("Sitting on plate");
+        }
+    }
+    private void Awake() {
+        presCubeRb = presentCube.GetComponent<Rigidbody2D>();
+
+        pastCube.SetActive(false);
+        presentCube.SetActive(false);
+        futureCube.SetActive(false);        
+    }
+    void Start()
+    {
+        initialPos = transform.position;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveUp)
+        {
+            if(transform.position.y < initialPos.y && Time.time - startTime > 3.0)
+                transform.Translate(0, -pressWeight, 0);
+            else if(transform.position.y >= initialPos.y)
+                moveUp = false;
+        }
+    }
+}
