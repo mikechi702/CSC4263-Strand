5ac5ddc1edc74bc9d9dd0ad77465fa5551133cc7
smirkyface <72003137+smirkyface@users.noreply.github.com>
2023-05-08 20:29:55 -0500

    Update portalScript.cs

--- Assets/Scripts/SceneScripts/portalScript.cs
+++ Assets/Scripts/SceneScripts/portalScript.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class portalScript : MonoBehaviour
+public class PortalScript : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) 
+    public GameObject loadingScreen;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
         Scene currentScene = SceneManager.GetActiveScene();
 
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            switch(currentScene.name) //Whenever new level is added, ADD IT TO THHE BUILD SETTINGS !!!
+            switch (currentScene.name) //Whenever new level is added, ADD IT TO THHE BUILD SETTINGS !!!
             {
-                case("SampleScene"):
-                    SceneManager.LoadScene("SampleScene2");
+                case ("SampleScene"):
+                    StartCoroutine(LoadLevelAsync("SampleScene2"));
                     break;
-                case("SampleScene2"):
-                    SceneManager.LoadScene("EndScreen");
+                case ("SampleScene2"):
+                    StartCoroutine(LoadLevelAsync("TitleScreen"));
                     break;
             }
+        }
+    }
+
+    IEnumerator LoadLevelAsync(string scene)
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+        Time.timeScale = 1;
+        Slider progressBar = loadingScreen.transform.Find("RawImage").Find("Slider").gameObject.GetComponent<Slider>();
+
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log("Loading progress: " + (asyncLoad.progress * 100) + "%");
+            progressBar.value = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+
+            yield return null;
         }
     }
 }
