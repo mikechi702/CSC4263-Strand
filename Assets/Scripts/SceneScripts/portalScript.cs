3f35f6192c888fd5e7dcff35b4b52a45c784edc8
mikechi702 <mikechicarroll702@gmail.com>
2023-09-06 12:13:47 -0500

    Revert "Update portalScript.cs"
    This reverts commit 5ac5ddc1edc74bc9d9dd0ad77465fa5551133cc7.

--- Assets/Scripts/SceneScripts/portalScript.cs
+++ Assets/Scripts/SceneScripts/portalScript.cs
@@ -3,41 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PortalScript : MonoBehaviour
+public class portalScript : MonoBehaviour
 {
-    public GameObject loadingScreen;
-
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) 
     {
         Scene currentScene = SceneManager.GetActiveScene();
 
-        if (other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))
         {
-            switch (currentScene.name) //Whenever new level is added, ADD IT TO THHE BUILD SETTINGS !!!
+            switch(currentScene.name) //Whenever new level is added, ADD IT TO THHE BUILD SETTINGS !!!
             {
-                case ("SampleScene"):
-                    StartCoroutine(LoadLevelAsync("SampleScene2"));
+                case("SampleScene"):
+                    SceneManager.LoadScene("SampleScene2");
                     break;
-                case ("SampleScene2"):
-                    StartCoroutine(LoadLevelAsync("TitleScreen"));
+                case("SampleScene2"):
+                    SceneManager.LoadScene("EndScreen");
                     break;
             }
-        }
-    }
-
-    IEnumerator LoadLevelAsync(string scene)
-    {
-        loadingScreen.SetActive(true);
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
-        Time.timeScale = 1;
-        Slider progressBar = loadingScreen.transform.Find("RawImage").Find("Slider").gameObject.GetComponent<Slider>();
-
-        while (!asyncLoad.isDone)
-        {
-            Debug.Log("Loading progress: " + (asyncLoad.progress * 100) + "%");
-            progressBar.value = Mathf.Clamp01(asyncLoad.progress / 0.9f);
-
-            yield return null;
         }
     }
 }
